30a7b923e73f35653f2905d39ec05c9c41af8a65
Nicholas Porter <njporter10@gmail.com>
2016-07-07 14:58:55 -0700

    mem match v0.5 - Nick Porter

--- Memory Script.js
+++ Memory Script.js
@@ -5,13 +5,50 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matched = 2;
 var match_counter = 0;
-
+var timesClicked = 0;
 $(document).ready(function(){
-   $('.card-contain').one('mouseup',function card_clicked(){
+   $('.card-contain').on('mouseup',function card_clicked(event) {
        $(this).children('.back').css('z-index', '1');
-       first_card_clicked = $(this).children('img').className;
-       console.log(first_card_clicked);
-       });
+       first_card_clicked = 'clicked';
+       timesClicked++;
+        if(timesClicked > 1){
+            alert('try again');
+            $('.back').css('z-index', '0');
+            timesClicked = 0;
+            
+        }
+       if (first_card_clicked != null) {
+           first_card_clicked = $(this).children($('.back')).children($('img')).attr('class');
+           console.log(first_card_clicked);
+
+
+       }
+
+
+   });
+
+
+
+
+
+       // else{
+       //     second_card_clicked = $(this);
+       //     if(first_card_clicked == second_card_clicked){
+       //         match_counter++;
+       //         first_card_clicked=null;
+       //         second_card_clicked=null;
+       //         if(match_counter===total_possible_matched){
+       //             alert('you won');
+       //         }
+       //         else{
+       //
+       //         }
+       //     }
+       //     else{
+       //
+       //     }
+       // }
+       // });
 
 
 
