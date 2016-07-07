443c97dbdb2a2f89b5162508b852f74c5b8e5142
Brian Phan <brianphan88@gmail.com>
2016-07-07 11:03:44 -0700

    v0.1 - Brian Phan

---
+++ game-functions.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function(){
+
+    var cardObj = {};
+    
+    for(var i = 0; i < 30; i++) {
+
+        cardObj[i] = {
+            id: i,
+            imgLink: 'images/stella.jpg',
+
+        };
+
+    }
+    console.log(card[i]);
+});
