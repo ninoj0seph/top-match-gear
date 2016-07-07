6a7f45a8af4b478115a59dc9f95c52fa4a4e75fc
Nicholas Porter <njporter10@gmail.com>
2016-07-07 15:33:59 -0700

    mem match - Nick Porter

--- Memory Script.js
+++ Memory Script.js
@@ -7,20 +7,29 @@ var total_possible_matched = 2;
 var match_counter = 0;
 var timesClicked = 0;
 $(document).ready(function(){
-   $('.card-contain').on('mouseup',function card_clicked(event) {
+   $('.card-contain').on('mouseup',function card_clicked() {
        $(this).children('.back').css('z-index', '1');
-       first_card_clicked = 'clicked';
        timesClicked++;
-       first_card_clicked = $(this).children($('.back')).children($('img')).attr('class');
-       console.log(first_card_clicked);
-
+       if(timesClicked === 1){
+           first_card_clicked = $(this).children($('.back')).children($('img')).attr('class');
+           console.log(first_card_clicked);
+       }
        if(timesClicked > 1){
-            alert('try again');
-            $('.back').css('z-index', '0');
+            $(this).css('z-index', '1');
             timesClicked = 0;
             second_card_clicked = $(this).children($('.back')).children($('img')).attr('class');
-            console.log(second_card_clicked)
+            console.log(second_card_clicked);
+                if(first_card_clicked==second_card_clicked){
+                    alert('Great Match');
+                    $(this).remove();
+                }
+                else{
+                    alert("Try Again")
+                    $('.back').css('z-index', '0');
+                }
+
         }
+
        // if (first_card_clicked != null) {
        //     first_card_clicked = $(this).children($('.back')).children($('img')).attr('class');
        //     console.log(first_card_clicked);
