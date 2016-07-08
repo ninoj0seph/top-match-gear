3e2116b5f5dfe5a22b2018ff173fb3f43b5ad4c5
emalfano <emalfano1@gmail.com>
2016-07-08 11:54:56 -0700

    Memory Match v0.5 - Elizabeth Alfano

--- main.js
+++ main.js
@@ -3,48 +3,57 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matches = 2;
 var match_counter = 0;
-
-//declare a new function
-function card_clicked(front,back){
-    //$(elem).hide();
-    if (first_card_clicked == null){
-        first_card_clicked = front;
-        console.log("first card clicked= ",first_card_clicked);
-        return;
-    } else {
-        second_card_clicked = front;
-        console.log("second card clicked= ",second_card_clicked);
-        if (first_card_clicked == second_card_clicked) {
-            match_counter += 1;
-            first_card_clicked = null; second_card_clicked = null;
-            console.log(" first_card_clicked ",  first_card_clicked, "second_card_clicked ", second_card_clicked);
-            if (match_counter == total_possible_matches) {
-                alert("You won!");
-            } else {
-                alert("Not done yet");
-                return;
-            }
-        } else {
-            setTimeout(showBack,3000);
-            function  showBack(){
-                alert("what now?");
-            }
-            //$('.card').attr('src',back);
-            return;
-        }
-    }
-}
+var you_won = "<p style='font-size:50px; position:relative; left:20vw;'>You won!</p>"
 
 $(document).ready(function(){
     //.click is like using the onclick attribute we used before
     //It creates an event handler for when div1 is clicked
     //Add click method to a jQuery Selector with the parameter equal to a function called card_clicked
     $(".card").on("click", function(){
-        $(this).find('.back').hide();
-        var front = $(this).find('.front').html();
-        var back = $(this).find('.back').html();
+        hideBack($(this));
         //console.log('front will be ',front);
         //console.log('back will be ',back);
-        card_clicked(front,back);
+        card_clicked($(this));
     });
 });
+
+//declare a new function
+function card_clicked(clicked_card){
+    if (first_card_clicked == null){
+        first_card_clicked = clicked_card;
+        return;
+    } else {
+        second_card_clicked = clicked_card;
+        if (first_card_clicked.find('.front img').attr('src') == second_card_clicked.find('.front img').attr('src')) {
+            match_counter += 1;
+            first_card_clicked = null; second_card_clicked = null;
+            if (match_counter == total_possible_matches) {
+                $('#game-area').html(you_won);
+            } else {
+                return;
+            }
+        } else {
+            disableClicks();
+            setTimeout(showBack,2000);
+            setTimeout(enableClicks,2000);
+            return;
+        }
+    }
+}
+
+function  showBack(){
+    first_card_clicked.find('.back').show();
+    second_card_clicked.find('.back').show();
+    first_card_clicked = null;
+    second_card_clicked = null;
+}
+
+function hideBack(card) {
+    $(card).find('.back').hide();
+}
+function disableClicks() {
+    $(document.body).css('pointer-events','none');
+}
+function enableClicks() {
+    $(document.body).css('pointer-events','auto');
+}
