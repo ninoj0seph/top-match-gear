be363d7557a2f572397bf5084431d91c8245c728
rosemariegonzales <rmbgonzales@gmail.com>
2016-07-08 11:48:43 -0700

    Memory Match v0.5 - Rosemarie Gonzales

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 /*Created by Rosemarie Gonzales on 7/6/16.*/
 
 $(document).ready(function () {
+    //When a card is clicked, execute the following code:
     $(".card").click(function () {
         card_clicked($(this));
     });
+    //When the 'Reset Game' button is clicked, execute the following code:
     $(".reset").click(function () {
         location.reload();
     });
@@ -13,32 +15,44 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matches = 2;
 var match_counter = 0;
+var card_back1 = null;
+var card_back2 = null;
+
 
 function card_clicked(clicked_card) {
-    //Flip the back of the card to show card face
-    var card_back = clicked_card.find(".back");
-    card_back.hide();
 
-    //Get image source to be assigned to first_card_clicked or second_card_clicked
-    var clicked_card1 = clicked_card.children("div.front");
-    var card_src = clicked_card1.find("img").attr("src");
-    console.log(card_src);
+    /* Previous code..Keep for future referece:
+     var card_back = clicked_card.find(".back");
+     card_back.hide();*/
 
-    //Assign card_src to first_card clicked
+    //Get <div> element with class of .back for assignment to either card_back1 or card_back2.
+    var card_back = clicked_card.children("div.back");
+
+    //Get image source to be assigned to first_card_clicked or second_card_clicked.
+    var card_src = clicked_card.children("div.front").find("img").attr("src");
+
+    //Assign card_back and card_src to first_card clicked.
     if (first_card_clicked == null) {
-        first_card_clicked = card_src;
-        console.log(first_card_clicked);
-        return first_card_clicked;
-    }
-    //Assign card_src to second_card_clicked
-    else {
-        second_card_clicked = card_src;
-        console.log(second_card_clicked);
+        card_back1 = card_back;
+        console.log(card_back1.html());//to check the value assigned to card_back1
+        card_back1.hide();//show card face
 
-        //Test to see if cards clicked match
+        first_card_clicked = card_src;
+        console.log(first_card_clicked);//to check the value assigned to first_card_clicked
+    }
+    //Assign card_back and card_src to second_card_clicked.
+    else {
+        card_back2 = card_back;
+        console.log(card_back2.html());//to check the value assigned to card_back2
+        card_back2.hide();
+
+        second_card_clicked = card_src;
+        console.log(second_card_clicked);//to check the value assigned to second_card_clicked
+
+        //Test to see if cards clicked match.
         if (first_card_clicked == second_card_clicked) {
 
-            //Increment match_counter by 1
+            //Increment match_counter by 1.
             match_counter++;
             console.log(match_counter);
 
@@ -55,17 +69,19 @@ function card_clicked(clicked_card) {
             }
         }
         else {
- /*           var reset_card = function(){
+            //Function to return the back of the cards since they don't match
+            var show_card_back = function () {
                 card_back1.show();
                 card_back2.show();
             };
-            setTimeout(reset_card, 2000);*/
+            //Wait for 2 seconds then run the reset_card function
+            setTimeout(show_card_back, 2000);
+
+            //Reset the values of first_card_clicked and second_card_clicked
             first_card_clicked = null;
-            console.log(first_card_clicked);
+            console.log(first_card_clicked);//to check that value is back to null
             second_card_clicked = null;
-            console.log(second_card_clicked);
-            return first_card_clicked;
-            return second_card_clicked;
+            console.log(second_card_clicked);//to check that value is back to null
         }
     }
 }
