d02d8f0a1cfc396cf34f7b429b6f4bb9316b5a8e
Joshua Byun <byunsung89@gmail.com>
2016-07-08 18:11:41 -0700

    v0.5

--- script.js
+++ script.js
@@ -7,53 +7,51 @@ var total_possible_matches = 2; //total possible matches
 var match_counter = 0;
 var two_cards_clicked = false;
 
-$(document).ready( function(){
-    $('.card').on('click',card_clicked);
+$(document).ready(function () {
+    $('.card').on('click', card_clicked);
     // $(".card").click(card_clicked);
 });
-function card_clicked(e){
+function card_clicked(e) {
     console.log(e);//e is the event object
-    if(two_cards_clicked){
+    if (two_cards_clicked) {
         return;
     }
-    if(first_card_clicked == null){
+    if (first_card_clicked == null) {
         $(this).find('.back').hide();
         first_card_clicked = $(this);
         console.log("first card back hidden");
         first_card_clicked.off('click');
     }
-    else{
+    else {
         two_cards_clicked = true;
         $(this).find('.back').hide();
         second_card_clicked = $(this);
         console.log("second card back hidden");
-        if(first_card_clicked.find('.front').find('img').attr('src') == second_card_clicked.find('.front').find('img').attr('src')){
+        if (first_card_clicked.find('.front').find('img').attr('src') == second_card_clicked.find('.front').find('img').attr('src')) {
             two_cards_clicked = false;
-            match_counter +=1;
+            match_counter += 1;
             console.log("cards matched!");
             second_card_clicked.off('click');
             //the event handler 'click' is on 'card' element which the event was triggered. (this)
             console.log("event handlers on matched card removed");
             //remove event handler off();
-            if(total_possible_matches == match_counter){
+            if (total_possible_matches == match_counter) {
                 var hello = $('<div>').addClass("you_won").html("You Won WOOHOO!")
                 $('#game-area').append(hello);
-                $('.card').find('.back').show();
             }
-            else{
+            else {
                 first_card_clicked = null;
                 second_card_clicked = null;
                 return;
             }
         }
-        else{
-            setTimeout(wait_two_seconds,2000);
+        else {
+            setTimeout(reset_values, 2000);
             return;
         }
     }
 }
-
-function wait_two_seconds(){
+function reset_values() {
     two_cards_clicked = false;
     first_card_clicked.on('click', card_clicked);
     second_card_clicked.on('click', card_clicked);
