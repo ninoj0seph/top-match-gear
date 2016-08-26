2f683cd8f761589e4fb6b50741ed934df2ae22b8
Collette-Tamez <cttmz4@gmail.com>
2016-08-26 13:16:21 -0700

    stopped function from breaking with multiple clicks on same element, started time out function, added victory state

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ function card_clicked(event) {
     var cardClicked = $(this);
     cardClicked.find(".back").addClass("hidden");
     console.log("CARD FLIPPED");
-    if (first_card_clicked == null) {
-        first_card_clicked = cardClicked;
+    if (first_card_clicked == null && !cardClicked.hasClass("revealed")) {
+        first_card_clicked = cardClicked.addClass("revealed");
         console.log("FIRST CARD");
+    } else if (cardClicked.hasClass("revealed")){
+        return;
     } else {
-        second_card_clicked = $(this);
+        second_card_clicked = $(this).addClass("revealed");
         console.log("SECOND CARD");
         if (first_card_clicked.find(".front > img").attr('src') === second_card_clicked.find(".front > img").attr('src') && match_counter < total_possible_matches) {
             console.log("THESE CARDS MATCH");
@@ -23,22 +25,37 @@ function card_clicked(event) {
             first_card_clicked = null;
             second_card_clicked = null;
             if (match_counter === total_possible_matches) {
-                ////this bit needs to be made fancy
-                console.log("you won");
+                $(".card").css({
+                    display: "none"
+                });
+                $("#game-area").append("<p>YOU WON</p>");
+                $("<img>").attr({
+                    src: "assets/mgsvr1.jpg"
+                }).css({
+                    margin: "auto",
+                    display: "table"
+                }).appendTo("#game-area");
             }
         } else {
-            ////how wait
+            ////setTimeout(cardClicked)
             ////how negate other clicks
-            console.log("wait");
-            $(first_card_clicked).find(".back").removeClass("hidden");
-            console.log("FLIPPED BACK OVER");
-            $(second_card_clicked).find(".back").removeClass("hidden");
-            first_card_clicked = null;
-            second_card_clicked = null;
-            return console.log("try again");
+            console.log("BEFORE TIMER");
+            (console.log("and now we wait"));
+            setTimeout(resetState, 3000);
+            console.log("AFTER TIMER");
         }
     }
     console.log("FUNCTION END");
+}
+
+function resetState(){
+    console.log("TIMER IS DONE");
+    $(first_card_clicked).removeClass("revealed").find(".back").removeClass("hidden");
+    console.log("FLIPPED BACK OVER");
+    $(second_card_clicked).removeClass("revealed").find(".back").removeClass("hidden");
+    first_card_clicked = null;
+    second_card_clicked = null;
+    return console.log("try again");
 }
 $(document).ready(function(){
     console.log("DOC LOADED");
