c54ae69ce95229c720ce81145374661c7b59b56b
Collette-Tamez <cttmz4@gmail.com>
2016-08-26 11:19:49 -0700

    refined function, now executes as intended

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ var match_counter = 0;
 ////function to execute when clicked
 function card_clicked(event) {
     console.log("FUNCTION START");
-    var cardback1 = $(this).addClass("hidden");
+    var cardClicked = $(this);
+    cardClicked.find(".back").addClass("hidden");
     console.log("CARD FLIPPED");
-    ////the first card should be null then defined. how do i do that?
     if (first_card_clicked == null) {
-        first_card_clicked = cardback1;
+        first_card_clicked = cardClicked;
+        console.log("FIRST CARD");
     } else {
         second_card_clicked = $(this);
-        ////how do i accurately check if the images have the same source
-        if (first_card_clicked.children('img').attr('src') === second_card_clicked.children('img').attr('src') && match_counter < 3) {
+        console.log("SECOND CARD");
+        if (first_card_clicked.find(".front > img").attr('src') === second_card_clicked.find(".front > img").attr('src') && match_counter < total_possible_matches) {
             console.log("THESE CARDS MATCH");
             match_counter++;
             console.log(match_counter);
@@ -24,16 +25,14 @@ function card_clicked(event) {
             if (match_counter === total_possible_matches) {
                 ////this bit needs to be made fancy
                 console.log("you won");
-            } else {
-               ///how breakout without the console log?
-                console.log("keep matching");
             }
         } else {
             ////how wait
             ////how negate other clicks
             console.log("wait");
-            $(first_card_clicked).removeClass("hidden");
-            $(second_card_clicked).removeClass("hidden");
+            $(first_card_clicked).find(".back").removeClass("hidden");
+            console.log("FLIPPED BACK OVER");
+            $(second_card_clicked).find(".back").removeClass("hidden");
             first_card_clicked = null;
             second_card_clicked = null;
             return console.log("try again");
@@ -43,11 +42,7 @@ function card_clicked(event) {
 }
 $(document).ready(function(){
     console.log("DOC LOADED");
-   $(".back").click(card_clicked);
+   $(".card").click(card_clicked);
 });
 
-/*
- ($(this).children('img').attr('src'));
- */
 
-//if click count = 1 give class of whatever?
