6dfecb2ab5503d0041aa18aca92b08e606552bba
Dave Weizenegger <Dave Weizenegger>
2016-08-26 17:32:08 -0700

    add/remove clickability and working on reset button now

--- myScript.js
+++ myScript.js
@@ -7,13 +7,15 @@ $(document).ready(function () {
 
     randomImg();
     gamePlay();
-    $(".card").flip();
+    $(".card").flip({
+        trigger: 'manual'
+    });
 });
 
 // Declare all of the global variables
 var cardArray = [1, 2]; //, 3, 4, 5, 6, 7, 8, 9];
 cardArray = cardArray.concat(cardArray);
-
+var clickable = true;
 var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matches = 2;
@@ -23,13 +25,12 @@ var match_counter = 0;
 function randomImg() {
 
     var ranArray = [];
-
-    while (cardArray.length != 0) {
-        var ran = Math.floor(Math.random() * (cardArray.length - 1) - 1) + 1;
-        ranArray.push(cardArray[ran]);
-        cardArray.splice(ran, 1);
+    var newCardArray = cardArray.slice();
+    while (newCardArray.length != 0) {
+        var ran = Math.floor(Math.random() * (newCardArray.length - 1) - 1) + 1;
+        ranArray.push(newCardArray[ran]);
+        newCardArray.splice(ran, 1);
     }
-
 
     $('.back').each(function (val) {
 
@@ -43,39 +44,52 @@ function randomImg() {
 // Gameplay code
 function gamePlay() {
 
+
     $(".card").click(function () {
-        if($(this).hasClass("revealed")){
+
+        if ($(this).hasClass("revealed")) {
             $(this).flip(false);
+
             return;
         }
-
+        if (clickable == false) {
+            $(this).flip(false);
+            console.log("what is clickable???", clickable);
+            return;
+        }
+        $(this).flip(true);
         $(this).addClass("revealed");
         if (first_card_clicked == null) {
             first_card_clicked = $(this);
-            //.children(".back").css('background')
+
         } else {
             second_card_clicked = $(this);
-            //.children(".back").css('background')
+
             var first_card_background = first_card_clicked.children(".back").css('background');
             var second_card_background = second_card_clicked.children(".back").css('background');
 
-           if(second_card_background == first_card_background){
+            if (second_card_background == first_card_background) {
 
                 matched();
 
-            } else{
-                console.log("TRY AGAIN!");
+            } else {
+                clickable = false;
+                console.log("Just before not_matched function, is it clickable: ", clickable);
                 setTimeout(not_matched, 1500);
             }
         }
+
     });
+
 }
 
-function matched(){
-    match_counter = match_counter +1;
-    if(match_counter == total_possible_matches){
+function matched() {
+    match_counter = match_counter + 1;
+
+    console.log("In matched function, is it clickable: ", clickable);
+    if (match_counter == total_possible_matches) {
         $("#footer p").text("YOU WIN!!!");
-    }else{
+    } else {
 
         $("#footer p").text("GOOD JOB, KEEP GOING!");
     }
@@ -83,18 +97,29 @@ function matched(){
 
 }
 
-function not_matched(){
-    first_card_clicked.flip(false);
-    second_card_clicked.flip(false);
+function not_matched() {
+    first_card_clicked.removeClass('revealed').flip(false);
+    second_card_clicked.removeClass('revealed').flip(false);
     first_card_clicked = null;
     second_card_clicked = null;
+    clickable = true;
+    $(".card").flip(false);
+    console.log("In not_matched function, is it clickable: ", clickable);
     $("#footer p").text("WHOOPS, WRONG!!");
 }
 
-function stopFlip(){
+function stopFlip() {
     first_card_clicked.unbind("click");
     second_card_clicked.unbind("click");
     first_card_clicked = null;
     second_card_clicked = null;
+
+}
+
+function reset() {
+    $(".card").flip(false);
+    randomImg();
+    gamePlay();
+    $(".card").flip();
 
 }
