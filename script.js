1ff7609645af2814b1bdb078555f3d3ced0c07df
Collette-Tamez <cttmz4@gmail.com>
2016-08-26 18:08:58 -0700

    declared new variables, created skeleton for functions to be fleshed out, placed images in new sub directory, added rest of deck

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
+//TODO randomize cards, add flip effects?, and a sound effect or two, get stats to appear and update, make button work, play again button??? fix game aread width, FIGURE OUT HOW TO MATH. add to reset button.
 
 ////global variables
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
-var match_counter = 0;
+var total_possible_matches = 9;
 var canClick = true;
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+
 ////function to execute when clicked
 function card_clicked(event) {
-    console.log("FUNCTION START");
+    ///console.log("FUNCTION START");
     var thisCard = $(event);
     thisCard.find(".back").addClass("hidden");
-    console.log("CARD FLIPPED");
+    ///console.log("CARD FLIPPED");
     if (first_card_clicked == null && !thisCard.hasClass("revealed")) {
         first_card_clicked = thisCard.addClass("revealed");
-        console.log("FIRST CARD");
+        ///console.log("FIRST CARD");
     } else if (thisCard.hasClass("revealed")){
         return;
     } else {
         second_card_clicked = $(event).addClass("revealed");
-        console.log("SECOND CARD");
-        if (first_card_clicked.find(".front > img").attr('src') === second_card_clicked.find(".front > img").attr('src') && match_counter < total_possible_matches) {
-            console.log("THESE CARDS MATCH");
-            match_counter++;
-            console.log(match_counter);
+        attempts++;
+        ///console.log("SECOND CARD");
+        if (first_card_clicked.find(".front > img").attr('src') === second_card_clicked.find(".front > img").attr('src') && matches < total_possible_matches) {
+            ///console.log("THESE CARDS MATCH");
+            matches++;
+            ///console.log(match_counter);
             first_card_clicked = null;
             second_card_clicked = null;
-            if (match_counter === total_possible_matches) {
+            if (matches === total_possible_matches) {
                 $(".card").css({
                     display: "none"
                 });
@@ -40,34 +45,54 @@ function card_clicked(event) {
         } else {
             ////how negate other clicks
             canClick = false;
-            (console.log("and now we wait"));
+            ///(console.log("and now we wait"));
             setTimeout(resetState, 2000);
         }
     }
-    console.log("FUNCTION END");
+    ///console.log("FUNCTION END");
 }
 ///FUNCTION TO RESET MISMATCH
 function resetState(){
-    console.log("START TIMER");
+   /// console.log("START TIMER");
     first_card_clicked.removeClass("revealed").find(".back").removeClass("hidden");
-    console.log("FLIPPED BACK OVER");
+   /// console.log("FLIPPED BACK OVER");
     second_card_clicked.removeClass("revealed").find(".back").removeClass("hidden");
     first_card_clicked = null;
     second_card_clicked = null;
-    console.log("TIMER IS DONE?");
-    console.log("try again");
+   /// console.log("TIMER IS DONE?");
+  ///  console.log("try again");
     canClick = true;
 }
-////CLICK HANDLER
+////CARD CLICK HANDLER
 $(document).ready(function () {
-    console.log("DOC LOADED");
-    console.log("you can click");
+    ///console.log("DOC LOADED");
+    ///console.log("you can click");
+    var games_played = 0; ///add to reset button!!!
     $(".card").click(function () {
         if (canClick){
            card_clicked(this);
         } else {
-            return console.log("no good");
+            return console.log("stop it");
         }
     });
 });
-
+////STATS FUNCTION
+function display_stats() {
+    ///gamesplayes -> .games-played .value
+    ///attempts ->attempts .value
+    ///format accuracy = (matches/attempts)% HOW MATH WORKS?
+    ///accuracy ->.accuracy .value
+}
+function reset_stats() {
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+    display_stats();
+}
+///REST CLICK HANDLER
+$("<button>").click(function () {
+    games_played++;
+    reset_stats();
+    display_stats();
+    ///reset game area
+});
