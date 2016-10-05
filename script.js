f29420aa98b0b5d4d46025e5da944188a6f5b422
Kevin Chau <kchau.jobs@gmail.com>
2016-10-05 12:18:27 -0700

    commented code and separate reveal and hide into individual functions

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
-firstCardClicked = null;
-secondCardClicked = null;
-totalPossibleMatches = 3;
-matchCountered = 0;
+var firstCardClicked = null;
+var secondCardClicked = null;
+var totalPossibleMatches = 9;
+var matchCounter = 0;
+var cardsFlipped = 0;
+var firstCardClickedImg = null;
+var secondCardClickedImg = null;
 
 $(document).ready(function(){
-    $(".card").click(cardClicked);
-
-    function cardClicked(){
-        $(this).find(".back").hide();
-        if(firstCardClicked === null){
-            firstCardClicked = $(this).find(".front").attr("src");
-        }else{
-            secondCardClicked = $(this).find(".front").attr("src");
-            if(firstCardClicked === secondCardClicked){
-                matchCountered += 1;
-                firstCardClicked = null;
-                secondCardClicked = null;
-                if(matchCountered === totalPossibleMatches){
-                    alert("You won!");
-                }
-            }else{
-                setTimeout(function(){
-                    $("img[src$='"+firstCardClicked+"']").parent().find(".back").show();
-                    $("img[src$='"+secondCardClicked+"']").parent().find(".back").show();
-                    firstCardClicked = secondCardClicked = null;
-                }, 2000);
-            }
-        }
-
-    }
+    $(".card").on("click",cardClicked);
 });
+//calls function to reveal and hide front
+//check for matches, increase counter, check win condition
+function cardClicked(){
+    //prevent users from revealing all cards at once
+    if(cardsFlipped  < 2){
+        revealFront(this);
+        cardsFlipped +=1;
+    }else{//two cards already revealed
+        return false;
+    }
+    if(firstCardClicked === null){//Has first card been flipped?
+        firstCardClicked = this;
+        firstCardClickedImg = $(firstCardClicked).find(".front").attr("src");
+        console.log("first: ", firstCardClickedImg);
+    }else{//First card already flipped, must be second card
+        secondCardClicked = this;
+        secondCardClickedImg = $(secondCardClicked).find(".front").attr("src");
+        //is the img src of the two cards the same?
+        if(firstCardClickedImg === secondCardClickedImg){
+            cardsFlipped = 0;//reset number of cards clicked
+            matchCounter += 1;//increase match count by one
+            firstCardClicked = null;//clear cards clicked
+            secondCardClicked = null;
+            if(matchCounter === totalPossibleMatches){//Win condition
+                alert("You won!");
+            }
+        }else{//Cards img src don not match
+            setTimeout(function(){
+                //hide card fronts with card backs
+                coverFront(firstCardClicked);
+                coverFront(secondCardClicked);
+                firstCardClicked = secondCardClicked = null;
+                cardsFlipped = 0;
+            }, 2000);
+        }
+    }
 
+}
+function revealFront(theCard){
+    $(theCard).find(".back").hide();
+}
+function coverFront(theCard){
+    $(theCard).find(".back").show();
+}
