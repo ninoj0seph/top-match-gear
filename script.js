9d219630a1406e33528b6411b229fefd1ee4905d
Kevin Chau <kchau.jobs@gmail.com>
2016-10-05 13:54:39 -0700

    display stats and reset functionality added

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var firstCardClicked = null;
 var secondCardClicked = null;
-var totalPossibleMatches = 9;
-var matchCounter = 0;
-var cardsFlipped = 0;
 var firstCardClickedImg = null;
 var secondCardClickedImg = null;
+var attemptNum = 0;
+var matchAccuracy = 0;
+var gamePlayed = 0;
+var matchCounter = 0;
+var cardsFlipped = 0;
+var totalPossibleMatches = 9;
 
 $(document).ready(function(){
     $(".card").on("click",cardClicked);
+    displayStats();
+    $(".reset").click(resetStats);
 });
 //calls function to reveal and hide front
 //check for matches, increase counter, check win condition
@@ -22,8 +27,8 @@ function cardClicked(){
     if(firstCardClicked === null){//Has first card been flipped?
         firstCardClicked = this;
         firstCardClickedImg = $(firstCardClicked).find(".front").attr("src");
-        console.log("first: ", firstCardClickedImg);
     }else{//First card already flipped, must be second card
+        attemptNum += 1;
         secondCardClicked = this;
         secondCardClickedImg = $(secondCardClicked).find(".front").attr("src");
         //is the img src of the two cards the same?
@@ -45,11 +50,23 @@ function cardClicked(){
             }, 2000);
         }
     }
-
+    matchAccuracy = ((matchCounter/attemptNum)*100).toFixed(2);
+    displayStats();
 }
 function revealFront(theCard){
     $(theCard).find(".back").hide();
 }
 function coverFront(theCard){
     $(theCard).find(".back").show();
+}
+function displayStats(){
+    $(".games-played .value").text(gamePlayed);
+    $(".attempts .value").text(attemptNum);
+    $(".accuracy .value").text(matchAccuracy+"%");
+}
+function resetStats(){
+    console.log("Resetting Stats");
+    matchAccuracy = matchCounter = attemptNum = 0;
+    displayStats();
+    $(".back").show();
 }
