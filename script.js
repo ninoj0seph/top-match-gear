f5adb98ac8718ba4b3e4337f35c07570b423219c
Kevin Chau <kchau.jobs@gmail.com>
2016-10-07 15:05:47 -0700

    implemented shuffle and dynamic card creation

--- script.js
+++ script.js
@@ -8,42 +8,70 @@ var gamePlayed = 0;
 var matchCounter = 0;
 var cardsFlipped = 0;
 var totalPossibleMatches = 9;
-var cardFrontImg = ["img/brook.png","img/chopper.jpg","img/franky.png","img/luffy.png","img/nami.png", "img/robin.png","img/sanji.png","img/usopp", "img/zoro.png","img/brook.png","img/chopper.jpg","img/franky.png","img/luffy.png","img/nami.png", "img/robin.png","img/sanji.png","img/usopp", "img/zoro.png"];
+//the deck that will be passed into createGameArea
+var cardFrontImg = ["img/brook.png", "img/brook.png",
+                    "img/chopper.png", "img/chopper.png",
+                    "img/franky.png", "img/franky.png",
+                    "img/luffy.png", "img/luffy.png",
+                    "img/nami.png", "img/nami.png",
+                    "img/robin.png", "img/robin.png",
+                    "img/sanji.png", "img/sanji.png",
+                    "img/usopp.png", "img/usopp.png",
+                    "img/zoro.png", "img/zoro.png"
+];
+var cardBackImg = "img/jollyRoger.png";
+var cardPerRow = 6;
 
 $(document).ready(function(){
-    $(".card").on("click",cardClicked);
+    createGameArea(cardFrontImg);
+    $("#game-area").on("click", ".card", cardClicked);
     displayStats();
     $(".reset").click(resetStats);
-    $(".shuffle").click(shuffle);
 });
+
 //calls function to reveal and hide front
 //check for matches, increase counter, check win condition
-function cardClicked(){
+function cardClicked()
+    {
     //prevent users from revealing all cards at once
     if(cardsFlipped  < 2){
         revealFront(this);
         cardsFlipped +=1;
-    }else{//two cards already revealed
+    }
+    else
+    {//two cards already revealed
         return false;
     }
-    if(firstCardClicked === null){//Has first card been flipped?
+
+    //Has first card been clicked and assigned a card?
+    if(firstCardClicked === null)
+    {
         firstCardClicked = this;
         firstCardClickedImg = $(firstCardClicked).find(".front").attr("src");
-    }else{//First card already flipped, must be second card
+    }
+    else
+    {//firstCardClicked !== null; Second card is being clicked
         attemptNum += 1;
         secondCardClicked = this;
         secondCardClickedImg = $(secondCardClicked).find(".front").attr("src");
         //is the img src of the two cards the same?
-        if(firstCardClickedImg === secondCardClickedImg){
+
+        if(firstCardClickedImg === secondCardClickedImg)
+        {
             cardsFlipped = 0;//reset number of cards clicked
             matchCounter += 1;//increase match count by one
             firstCardClicked = null;//clear cards clicked
             secondCardClicked = null;
-            if(matchCounter === totalPossibleMatches){//Win condition
+            //Win condition met?
+            if(matchCounter === totalPossibleMatches)
+            {
                 alert("You won!");
             }
-        }else{//Cards img src don not match
-            setTimeout(function(){
+        }
+        else
+        {//Cards img src don not match
+            setTimeout(function()
+            {
                 //hide card fronts with card backs
                 coverFront(firstCardClicked);
                 coverFront(secondCardClicked);
@@ -55,33 +83,76 @@ function cardClicked(){
     matchAccuracy = ((matchCounter/attemptNum)*100).toFixed(2);
     displayStats();
 }
-function revealFront(theCard){
+function revealFront(theCard)
+{
     $(theCard).find(".back").hide();
 }
-function coverFront(theCard){
+
+function coverFront(theCard)
+{
     $(theCard).find(".back").show();
 }
-function displayStats(){
+
+function displayStats()
+{
     $(".games-played .value").text(gamePlayed);
     $(".attempts .value").text(attemptNum);
     $(".accuracy .value").text(matchAccuracy+"%");
 }
-function resetStats(){
+
+function resetStats()
+{
     matchAccuracy = matchCounter = attemptNum = 0;
+    //increment number of games played
+    gamePlayed++;
+    //refreshes displayed stats with new values
     displayStats();
-    $(".back").show();
+    //Clear out game-area before adding new card arrangement
+    $("#game-area").empty();
+    createGameArea(cardFrontImg);
+}
+
+//creates a new game area of shuffle cards with 3 rows of 6 cards
+function createGameArea(deck)
+{
+    var cardsInDeck = deck.length - 1;
+    shuffle(deck, cardsInDeck);//shuffle the deck of images before setting up
+
+    //Loop the length of cardFrontImg array. i takes value of j at end of nested loop
+    for(var i = 0; i <= cardsInDeck; i)
+    {
+        var $newRow = $("<div>").addClass("row card-row");//new card row
+        //create 6 column of cards with fronts and backs. i passes j starting value.
+        for(var j = i; j < i + cardPerRow; j++)
+        {
+            var $cardBox = $("<div>").addClass("col-sm-2 card-col");//column/container for card
+            var $newCard = $("<div>").addClass("card");
+            var $frontImg = $("<img>").addClass("front").attr("src", cardFrontImg[j]);
+            var $backImg = $("<img>").addClass("back").attr("src", cardBackImg);
+
+            $newCard.append($frontImg, $backImg);
+            $cardBox.append($newCard);
+            $newRow.append($cardBox);
+        }
+        i = j;
+        $("#game-area").append($newRow);
+    }
+
 }
 //shuffles array of img src
-function shuffle(){
-    console.log("Array before", cardFrontImg);
+function shuffle(deck, deckSize)
+{
+    //console.log("Array before", cardFrontImg);
     var temp = null;
     var randomNum = 0;
-    for(var i = (cardFrontImg.length-1); i > 0; i--){
-        randomNum = Math.floor(Math.random()*(totalPossibleMatches*2));
-        temp = cardFrontImg[i];
-        cardFrontImg[i] = cardFrontImg[randomNum];
-        cardFrontImg[randomNum] = temp;
+
+    for(var i = deckSize; i > 0; i--)
+    {
+        randomNum = Math.floor(Math.random()*deckSize);
+        temp = deck[i];
+        deck[i] = deck[randomNum];
+        deck[randomNum] = temp;
     }
-    console.log("Array of img", cardFrontImg);
+    // console.log("Array of img", cardFrontImg);
+    // console.log("Length after shuffle", cardFrontImg.length)
 }
-//TODO need to assign img src to card fronts.
