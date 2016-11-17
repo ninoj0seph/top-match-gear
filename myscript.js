d4568ef42e2cf2dc4fdfac000ee3aff81fd60e4f
Tammyslau29 <Tammyslau29@gmail.com>
2016-11-16 18:53:59 -0800

    javascript working

--- myscript.js
+++ myscript.js
@@ -1,19 +1,41 @@
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
-var matching_counter = 0;
+var total_possible_matches = 9;
+var match_counter = 0;
 
 $(document).ready(function () {
     $(".card").click(card_clicked);
 });
 function card_clicked() {
-   $(this).find(".back").hide();
-    if (first_card_clicked = null) {
-       return first_card_clicked = $(this);
+    if ($(this).find(".back").is(":visible") == false) {
     } else {
-        return second_card_clicked = $(this);
-        if (first_card_clicked === second_card_clicked){
-            console.log("yay");
+        $(this).find(".back").hide();
+        if (first_card_clicked == null) {
+            first_card_clicked = $(this);
+        } else {
+            second_card_clicked = $(this);
+            if (first_card_clicked.find(".front > img").attr("src") ===
+                second_card_clicked.find(".front > img").attr("src")) {
+                match_counter++;
+                first_card_clicked = null;
+                second_card_clicked = null;
+                if (total_possible_matches == match_counter) {
+                    $("#winning_gif").show();
+                    $(".card").hide();
+                } else {
+                    return;
+                }
+            } else {
+                $(".card").unbind("click");
+                function time_out() {
+                    first_card_clicked.find(".back").show();
+                    second_card_clicked.find(".back").show();
+                    $(".card").click(card_clicked);
+                    first_card_clicked = null;
+                    second_card_clicked = null;
+                }
+                setTimeout(time_out, 2000)
+            }
         }
     }
 };
