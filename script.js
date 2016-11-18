39a6016726cdfacea3e4c4d8b39ac7008346abc7
Cung Tran <cung.n.tran@gmail.com>
2016-11-17 19:44:55 -0800

    updated v1.0- Cung Tran

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var second_card_clicked = null;
 var total_possible_matches = 2;
 var match_counter = 0;
 
+var matches = 0;
+var attempts = 0;//Every time a user attempts a match (clicks the 2nd card) the attempts should be incremented by 1
+var accuracy = 0;//Accuracy is defined as a percentage of matches / attempts
+
+var games_played = 0;
+
+
 $(document).ready (function() {
     console.log("loaded");
     $('.back').click(card_clicked);
@@ -16,10 +23,25 @@ function card_clicked () {
     }
     else {
         second_card_clicked = $(this);
+        //attempts
+        attempts++;
+        $('.attempts > .value').text(attempts);
+        console.log ('helloo');
+
+        console.log('result accurate');
+
         console.log('second card is clicked');
         if (($(first_card_clicked) .attr('class')) === ($(second_card_clicked).attr('class')))
         {
+            //matches counts
             match_counter++;
+            matches++;
+            $(".matching > .value").text(matches);
+            //accuracy
+            var accuracy = (matches/attempts)*100+('%');
+
+            $('.accuracy > .value').text(accuracy);
+
             first_card_clicked = null;
             second_card_clicked = null;
             if (total_possible_matches == match_counter) {
@@ -29,8 +51,10 @@ function card_clicked () {
                 }
         }
         else {
-                setTimeout(timeOut, 2000);
-                return;
+
+            setTimeout(timeOut, 2000);
+
+            return;
             }
         }
     }
@@ -41,3 +65,24 @@ function timeOut() {
     first_card_clicked = null;
     second_card_clicked = null;
 }
+//------------------version 1.0--------------
+
+
+
+// $(document).ready (function() {
+//     console.log("loaded");
+//     $('.back').click(card_matching);
+// });
+// function card_matching () {
+//     if (($(first_card_clicked).attr('class')) === ($(second_card_clicked).attr('class'))) {
+//         matches++;
+//         $(".matching > .value").text(matches);
+//     }return true;
+// }
+//
+// function accurate (){
+//     accuracy = matches/attempts;
+//     var result = accuracy;
+//     $('.accuracy > .value').text(result);
+// }
+// console.log (accurate);
