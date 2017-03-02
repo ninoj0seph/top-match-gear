aa0276abfabf5552c628a2524ce9062c081a273b
ninoj0seph <ninojoseph.tugade@gmail.com>
2017-03-02 10:02:01 -0800

    last stable version

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-var first_card_clicked = null;
-var second_card_clicked = null;
-var total_possible_matches = 10;
-var match_counter = 0;
+var settings = {
+    "first_card_clicked":{front:null,back:null},
+    "second_card_clicked" : {front:null,back:null},
+    total_possible_matches : 10
+}
+
+// var first_card_clicked = null;
+// var second_card_clicked = null;
+// var total_possible_matches = 10;
+// var match_counter = 0;
+var statistics = {
+    matches: 0,
+    attempts: 0,
+    accuracy: 0,
+    games_played: 0
+};
+
 
 $(document).ready(function (){
     click_handler();
@@ -9,10 +22,10 @@ $(document).ready(function (){
 
 function card_clicked() {
     $(this).addClass("flip");
+    statistics.attempts++;
     if(first_card_clicked === null){
         first_card_clicked = $(this).parent().find(".front img").attr("src");
         console.log(first_card_clicked);
-        return;
     } else {
         second_card_clicked = $(this).parent().find(".front img").attr("src");
         console.log(second_card_clicked);
@@ -22,14 +35,41 @@ function card_clicked() {
             second_card_clicked = null;
             setTimeout(function () {
                 if(total_possible_matches === match_counter){
+                    statistics.games_played++;
                     alert("You Win!");
                 }
-            }, 75);
+            }
+            , 75);
+
+        } else {
+            setTimeout(function () {
+                $(".back").removeClass("flip");
+                }
+                , 500);
+            first_card_clicked = null;
+            second_card_clicked = null;
         }
     }
+    display_stats();
 }
-
 
 function click_handler() {
     $(".back").click(card_clicked);
+    $(".resetGame").click(reset_stats);
+}
+
+function display_stats() {
+    $("#games-played .value").text(statistics.games_played);
+    console.log("worked");
+    $("#attempts .value").text(statistics.attempts);
+    $("#accuracy .value").text(statistics.accuracy);
+}
+
+function reset_stats() {
+    for(var i in statistics){
+        statistics[i] = 0;
+    }
+    first_card_clicked = null;
+    second_card_clicked = null;
+    display_stats();
 }
